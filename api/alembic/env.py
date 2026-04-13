f3a54721ca8da205d44af840e1db6309320f7c78
Jakub Banach <jakub.banach00@gmail.com>
2026-04-13 20:00:38 +0200

    fix(api): translate comment

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.config import settings
 from app.database import Base
-import app.models  # noqa: F401 – rejestruje modele w Base.metadata
+import app.models.task  # noqa: F401 – registers models in Base.metadata
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
